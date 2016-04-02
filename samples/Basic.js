70f5cf08752a0b46fe83fa1715291b7158f9aa8c
juspears <juspears@paypal.com>
2016-04-01 19:23:38 -0700

    added button type

--- samples/Basic.js
+++ samples/Basic.js
@@ -14,6 +14,7 @@ module.exports = {
                 buttons: [{
                     label: 'Cancel',
                     action: 'cancel',
+                    type:'reset',
                     buttonClass: 'btn'
                 }, {
                     label: 'Submit', action: 'submit',
